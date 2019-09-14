5c250ad7e45de995d6c0277aa3312c2faf555a41
debnathachintya <achintya.k.debnath@gmail.com>
2019-09-14 20:35:30 +0530

    second commit

--- src/main/java/E2EProject/E2EProject/baseFile.java
+++ src/main/java/E2EProject/E2EProject/baseFile.java
@@ -13,15 +13,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.markuputils.ExtentColor;
-import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 public class baseFile {
@@ -60,30 +54,23 @@ public class baseFile {
 		FileUtils.copyFile(srcFile, new File("./screenshot/" + TestName + ".png"));
 	}
 
-	@BeforeSuite
-	public void testSetup() {
-		htmlReporter = new ExtentHtmlReporter("./reports/extent.html");
-		extent = new ExtentReports();
-		extent.attachReporter(htmlReporter);
-	}
+	/*
+	 * @BeforeSuite public void testSetup() { htmlReporter = new
+	 * ExtentHtmlReporter("./reports/extent.html"); extent = new ExtentReports();
+	 * extent.attachReporter(htmlReporter); }
+	 */
 
-	@AfterMethod
-	public void getResult(ITestResult result) {
-		if (result.getStatus() == ITestResult.FAILURE) {
-			test.fail(MarkupHelper.createLabel(result.getName() + "Test Case Failed", ExtentColor.RED));
-			test.fail(result.getThrowable());
-		}
-		else if(result.getStatus() == ITestResult.SUCCESS) {
-			test.pass(MarkupHelper.createLabel(result.getName() + "Test Case Passed", ExtentColor.GREEN));
-		}
-		else {
-			test.skip(MarkupHelper.createLabel(result.getName() + "Test Case Passed", ExtentColor.YELLOW));
-			test.skip(result.getThrowable());
-		}
-	}
-
-	@AfterSuite
-	public void flushReport() {
-		extent.flush();
-	}
+	/*
+	 * @AfterMethod public void getResult(ITestResult result) { if
+	 * (result.getStatus() == ITestResult.FAILURE) {
+	 * test.fail(MarkupHelper.createLabel(result.getName() + "Test Case Failed",
+	 * ExtentColor.RED)); test.fail(result.getThrowable()); } else
+	 * if(result.getStatus() == ITestResult.SUCCESS) {
+	 * test.pass(MarkupHelper.createLabel(result.getName() + "Test Case Passed",
+	 * ExtentColor.GREEN)); } else {
+	 * test.skip(MarkupHelper.createLabel(result.getName() + "Test Case Passed",
+	 * ExtentColor.YELLOW)); test.skip(result.getThrowable()); } }
+	 * 
+	 * @AfterSuite public void flushReport() { extent.flush(); }
+	 */
 }
